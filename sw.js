c2b86d87ec7d4111af6dcff3c0c20ccd13f4abf6
RAMOS747 <162630347+RAMOS747@users.noreply.github.com>
2026-07-21 22:00:51 -0500

    primer sibida de archivos

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'runners-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
